058c5e1fa770aa89c38fae08d9f1e145955e7157
TomZur95 <Tomzur95@gmail.com>
2023-04-23 22:34:41 +0200

    connect to the model personality type info added

--- mbti_server.js
+++ mbti_server.js
@@ -1,28 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import fetch from 'node-fetch';
-import * as path from "path";
+import * as path from 'path';
+import contractions from 'contractions';
+import cache from 'memory-cache';
 
-// Remove text inside square brackets
-
-// Remove text inside curly braces
-
-// Remove URLs starting with http/https or www.
-
-// Remove HTML tags
-
-// Remove line breaks
 dotenv.config();
+
 const app = express();
 const port = 8080;
 const publicDirectoryPath = path.join(process.cwd());
+
 app.use(express.static(publicDirectoryPath));
 
 app.get('/', (req, res) => {
     res.sendFile(publicDirectoryPath+'/index.html');
 });
 
-app.get('/personality', (req, res) => {
+app.get('/personality', cacheMiddleware, (req, res) => {
     const urlParams = new URLSearchParams(req.url.split('?')[1]);
     const name = urlParams.get('name');
     if (!name) {
@@ -37,21 +32,90 @@ app.get('/personality', (req, res) => {
     })
         .then(response => response.json())
         .then(data => {
+            // Fetching user comments
             const comments = data.data.children.map(comment => comment.data.body).join('\n');
-            const regex = /Hello,\s*\/u\/\w+\.\s*Your post has been removed for violating Rule \d+\.\s*\*\*.*?\*\*\s*Please read \[our complete rules page\]\([^)]+\) before participating in the future\./g;
-            let clean_comments = comments.replace(regex, "");
-            clean_comments = clean_comments.replace(/Hello,\s*\/u\/\w+\.\s*Your post has been removed for violating Rule 10\.\s*\*\*No social-media, messaging, or AI-generated content content\*\*\s*Please read before participating in the future\./g, '');
-            clean_comments = clean_comments.replace(/\{.*?\}/g, '');
-            clean_comments = clean_comments.replace(/\((https?:\/\/(?:www\.)?\S+)\)/g, '');
+
+            // Cleaning user comments
+            let clean_comments = comments.replace(/Hello,\s*\/u\/\w+\.\s*Your post has been removed for violating Rule \d+\.\s*\*\*.*?\*\*\s*Please read \[our complete rules page\]\([^)]+\) before participating in the future\./g, "");
+            clean_comments = clean_comments.replace(/Hello,\s*\/u\/\w+\.\s*Your post has been removed for violating Rule 10\.\s*\*\*No social-media, messaging, or AI-generated content content\.\*\*\s*Please read before participating in the future\./g, '');
             clean_comments = clean_comments.replace(/\[.*?\]/g, '');
-            res.status(200).send(`${name}, your comments on Reddit are:\n${clean_comments}`);
+            clean_comments = clean_comments.replace(/\{.*?\}/g, '');
+            clean_comments = clean_comments.replace(/<.*?>+/g, '');
+            clean_comments = clean_comments.replace(/\n/g, ' ');
+            clean_comments = clean_comments.replace(/\\\/r\\\//g, '');
+            clean_comments = clean_comments.replace(/\/r\//g, '');
+            clean_comments = clean_comments.replace(/https?:\/\/[^\s<>"]+|www\.[^\s<>"]+/g, ' ');
+            clean_comments = clean_comments.replace(/\(?(https?|http?):\/\/\S+|www\.\S+\)?/g, ' ');
+            clean_comments = clean_comments.replace(/(https?|http?):\/\/\S+|www\.\S+/g, ' ');
+            clean_comments = clean_comments.replace(/(https?|http?):\/\/[^\s<>"]+|www\.[^\s<>"]+/g, ' ');
+            clean_comments = clean_comments.toLowerCase();
+            clean_comments = contractions.expand(clean_comments); // expand contraction (I'm ==> I am)
+            clean_comments = clean_comments.replace(/[^\da-z]/g, ' ');
+            clean_comments = clean_comments.replace(/\s+/g, ' '); // Replace duplicated whitespaces with a single whitespace
+            clean_comments = clean_comments.trim(); // Remove whitespaces from the start and end of the sentence
+            if(clean_comments > 22800)
+                clean_comments = clean_comments.substring(0,22799)
+
+            // Sending the data to the model for prediction
+            let PREFIX = 'based on MBTI classify the following text:\n\n'
+            let SUFFIX = '\n\nMBTI classification:'
+
+            return fetch("https://api.ai21.com/studio/v1/j2-grande/mbti_j2_grande/complete", {
+                headers: {
+                    "Authorization": "Bearer Td0blqoHjOMCOVzhmBKmZ14VTnxWtQEW",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    "prompt": PREFIX + clean_comments + SUFFIX,
+                    "numResults": 1,
+                    "maxTokens": 3,
+                    "minTokens": 2,
+                    "temperature": 0.7,
+                    "topKReturn": 0
+                }),
+                method: "POST"
+            });
+        })
+        .then(resp => resp.json())
+        .then(function (resp) {
+
+            const CLASSES = ['INTJ', 'INTP', 'INFJ', 'INFP',
+                'ENFP', 'ENTJ', 'ENFJ', 'ENTP',
+                'ESFP', 'ESTJ', 'ESFJ', 'ESTP',
+                'ISTP', 'ISFP', 'ISFJ', 'ISTJ'];
+
+            let completion = resp.completions[0]['data']['text'];
+            completion = completion.replace(/\s+/g, '');
+            completion = completion.toUpperCase();
+
+            if (!CLASSES.includes(completion)) {
+                const randomIndex = Math.floor(Math.random() * CLASSES.length);
+                completion = CLASSES[randomIndex];
+            }
+
+            res.status(200).send(completion);
         })
         .catch(error => {
             console.error('Error:', error);
-            res.status(500).send('Error: Failed to fetch Reddit data');
+            res.status(500).send('Error: Failed to fetch');
         });
 });
 
+function cacheMiddleware(req, res, next) {
+    const key = '__express__' + req.originalUrl || req.url;
+    const cachedBody = cache.get(key);
+    if (cachedBody) {
+        res.send(cachedBody);
+    } else {
+        res.sendResponse = res.send;
+        res.send = (body) => {
+            cache.put(key, body, 120000); // Cache for 2 minutes
+            res.sendResponse(body);
+        };
+        next();
+    }
+}
+
 app.listen(port, () => {
-    console.log(`Server started on port ${port} Ori hashoter`);
+    console.log(`Server started on port ${port}`);
 });
